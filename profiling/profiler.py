98d5a1d2d32c00430dce70bba32540d81f2ed62b
Chandu-Shree04 <136715196+Chandu-Shree04@users.noreply.github.com>
2026-05-26 14:04:47 +0530

    Add files via upload

---
+++ profiling/profiler.py
@@ -0,0 +1,12 @@
+def generate_profile(df):
+
+    profile = {
+        "rows": df.shape[0],
+        "columns": df.shape[1],
+        "missing_values": df.isnull().sum().to_dict(),
+        "duplicates": int(df.duplicated().sum()),
+        "data_types": df.dtypes.astype(str).to_dict(),
+        "summary": df.describe(include='all').to_dict()
+    }
+
+    return profile
